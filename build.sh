1c7523f280e8d220ae975642300787da4f6ea31e
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2023-09-16 19:14:43 -0400

    Use .tar.gz instead of .zip

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ rsync -a metadata.desktop .build/com.andyvoz.plasmoid.cyberghoststatus/
 rsync -a contents .build/com.andyvoz.plasmoid.cyberghoststatus/
 
 cd .build
-zip -r cyberghoststatus.zip com.andyvoz.plasmoid.cyberghoststatus
+tar -czvf cyberghoststatus.tar.gz com.andyvoz.plasmoid.cyberghoststatus
 cd -
